739c4918f1535c466ba8af14734096b8d5034fca
Szymon Budzinski <szbudzinski@gmail.com>
2025-01-08 22:02:30 +0100

    fixed GetAvailableAlleys access

--- src/main/java/com/example/demo/Security/Config/SecurityConfig.java
+++ src/main/java/com/example/demo/Security/Config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/api/profile").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                         .requestMatchers("/api/reservation/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
-                        .requestMatchers("/api/alley/availability").hasAuthority("ROLE_USER")
+                        .requestMatchers("/api/alley/available").hasAuthority("ROLE_USER")
                         .requestMatchers("/api/alley/**").hasAuthority("ROLE_ADMIN")
                         .requestMatchers(
                                 "/swagger-ui/**",
